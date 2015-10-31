aa2a40e12447a5e80f681a90c9b674ab792ac327
Ashley <ashley.wil@gmail.com>
2015-11-01 08:50:59 +1000

    added libatomic_ops and extgc as submodules

---
+++ vm/extgc/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ash/robovm/aura/vm/bdwgc/tests
+# Build directory: /home/ash/robovm/aura/vm/bdwgc/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
