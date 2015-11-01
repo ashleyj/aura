8c692b64e6f4056de862e15b4d749a1d49ca9ded
Ashley <ashley.wil@gmail.com>
2015-11-01 14:31:27 +1000

    Revert "added libatomic_ops and extgc as submodules"
    This reverts commit aa2a40e12447a5e80f681a90c9b674ab792ac327.

--- vm/extgc/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ash/robovm/aura/vm/bdwgc
-# Build directory: /home/ash/robovm/aura/vm/bdwgc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(tests)
--- vm/extgc/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ash/robovm/aura/vm/bdwgc/tests
-# Build directory: /home/ash/robovm/aura/vm/bdwgc/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
